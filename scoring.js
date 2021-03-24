b119caebe0d1404204fa3d49c51a7b166aaa20a0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-24 16:41:44 -0400

    feat function returns 0 when position is unknown

--- scoring.js
+++ scoring.js
@@ -1,5 +1,14 @@
 const calculateScore = () => {
+    if (this.position === 'QB') {
 
+    } else if (this.position === 'RB') {
+
+    } else if (this.position === 'WR') {
+
+    } else if (this.position === 'TE') {
+
+    }
+    return 0;
 }
 
 module.exports = calculateScore
