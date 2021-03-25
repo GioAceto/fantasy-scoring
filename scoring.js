ef7f025b52a850aa7d513068a1afb60bcc24f0bc
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-25 19:45:12 -0400

    refactored function

--- scoring.js
+++ scoring.js
@@ -2,26 +2,68 @@ calculateScore = (player) => {
     let result;
 
     if (player.position === 'QB') {
-        result = (player.stats.passing.yards / 25) + (player.stats.passing.touchdowns * 6) + (player.stats.passing.interceptions * -3) +
-            (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3);
+        let passing = player.stats.passing;
+        let rushing = player.stats.rushing;
+
+        result = (passing.yards / 25) +
+            (passing.touchdowns * 6) +
+            (passing.interceptions * -3) +
+            (rushing.yards / 10) +
+            (rushing.touchdowns * 6) +
+            (rushing.fumbles * -3)
+
     } else if (player.position === 'RB') {
-        result = (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3) +
-            (player.stats.receiving.receptions) + (player.stats.receiving.yards / 10) + (player.stats.receiving.touchdowns * 6) +
-            (player.stats.receiving.fumbles * -3) + (player.stats.return.kickreturn.yards / 15) + (player.stats.return.kickreturn.touchdowns * 6) +
-            (player.stats.return.kickreturn.fumbles * -3) + (player.stats.return.puntreturn.yards / 15) + (player.stats.return.puntreturn.touchdowns * 6) +
-            (player.stats.return.puntreturn.fumbles * -3)
+        let rushing = player.stats.rushing;
+        let receiving = player.stats.receiving;
+        let kickReturn = player.stats.return.kickreturn;
+        let puntReturn = player.stats.return.puntreturn;
+
+        result = (rushing.yards / 10) +
+            (rushing.touchdowns * 6) +
+            (rushing.fumbles * -3) +
+            (receiving.receptions) +
+            (receiving.yards / 10) +
+            (receiving.touchdowns * 6) +
+            (receiving.fumbles * -3) +
+            (kickReturn.yards / 15) +
+            (kickReturn.touchdowns * 6) +
+            (kickReturn.fumbles * -3) +
+            (puntReturn.yards / 15) +
+            (puntReturn.touchdowns * 6) +
+            (puntReturn.fumbles * -3)
+
     } else if (player.position === 'WR') {
-        result = (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3) +
-            (player.stats.receiving.receptions) + (player.stats.receiving.yards / 10) + (player.stats.receiving.touchdowns * 6) +
-            (player.stats.receiving.fumbles * -3) + (player.stats.return.kickreturn.yards / 15) + (player.stats.return.kickreturn.touchdowns * 6) +
-            (player.stats.return.kickreturn.fumbles * -3) + (player.stats.return.puntreturn.yards / 15) + (player.stats.return.puntreturn.touchdowns * 6) +
-            (player.stats.return.puntreturn.fumbles * -3)
+        let rushing = player.stats.rushing;
+        let receiving = player.stats.receiving;
+        let kickReturn = player.stats.return.kickreturn;
+        let puntReturn = player.stats.return.puntreturn;
+
+        result = (rushing.yards / 10) +
+            (rushing.touchdowns * 6) +
+            (rushing.fumbles * -3) +
+            (receiving.receptions) +
+            (receiving.yards / 10) +
+            (receiving.touchdowns * 6) +
+            (receiving.fumbles * -3) +
+            (kickReturn.yards / 15) +
+            (kickReturn.touchdowns * 6) +
+            (kickReturn.fumbles * -3) +
+            (puntReturn.yards / 15) +
+            (puntReturn.touchdowns * 6) +
+            (puntReturn.fumbles * -3)
+
     } else if (player.position === 'TE') {
-        result = (player.stats.receiving.receptions) + (player.stats.receiving.yards / 10) + (player.stats.receiving.touchdowns * 6) +
-            (player.stats.receiving.fumbles * -3)
+        let receiving = player.stats.receiving;
+
+        result = (receiving.receptions) +
+            (receiving.yards / 10) +
+            (receiving.touchdowns * 6) +
+            (receiving.fumbles * -3)
+
     } else {
         result = 0;
     }
+
     return result;
 }
 
