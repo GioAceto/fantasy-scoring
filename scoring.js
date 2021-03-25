004fa696e316746d81fe5712715e832f97dcc6d2
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-25 16:17:21 -0400

    change variable names for kick return and punt return

--- scoring.js
+++ scoring.js
@@ -1,16 +1,19 @@
 calculateScore = (player) => {
     let passing = player.stats.passing
     let rushing = player.stats.rushing
-    let receiving = this.stats.receiving
-    let returnKick = this.stats.return.kickreturn
-    let returnPunt = this.stats.return.puntreturn
-    let fieldgoals = this.stats.fieldgoals
-    let xp = this.stats.xp
+    let receiving = player.stats.receiving
+    let kickReturn = player.stats.return.kickreturn
+    let puntReturn = player.stats.return.puntreturn
+    let fieldgoals = player.stats.fieldgoals
+    let xp = player.stats.xp
 
     if (player.position === 'QB') {
-        return (passing.yards / 25) + (passing.touchdowns * 6) + (passing.interceptions * -3) + (rushing.yards / 10) + (rushing.touchdowns * 6) + (rushing.fumbles * -3)
+        return (passing.yards / 25) + (passing.touchdowns * 6) + (passing.interceptions * -3) +
+            (rushing.yards / 10) + (rushing.touchdowns * 6) + (rushing.fumbles * -3)
     } else if (player.position === 'RB') {
-
+        return (rushing.yards / 10) + (rushing.touchdowns * 6) + (rushing.fumbles * -3) +
+            (receiving.receptions) + (receiving.yards / 10) + (receiving.touchdowns * 6) +
+            (receiving.fumbles * -3) + (kickReturn)
     } else if (player.position === 'WR') {
 
     } else if (player.position === 'TE') {
