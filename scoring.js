62f5f785791980420958604f1c3205460b958765
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-25 18:57:32 -0400

    feat function returns score for a running back

--- scoring.js
+++ scoring.js
@@ -1,8 +1,9 @@
 calculateScore = (player) => {
     let result;
+
     if (player.position === 'QB') {
         result = (player.stats.passing.yards / 25) + (player.stats.passing.touchdowns * 6) + (player.stats.passing.interceptions * -3) +
-            (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3)
+            (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3);
     } else if (player.position === 'RB') {
         result = (player.stats.rushing.yards / 10) + (player.stats.rushing.touchdowns * 6) + (player.stats.rushing.fumbles * -3) +
             (player.stats.receiving.receptions) + (player.stats.receiving.yards / 10) + (player.stats.receiving.touchdowns * 6) +
