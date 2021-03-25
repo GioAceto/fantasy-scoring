3414a83337761140633e443de3aaee324348b071
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-25 14:52:56 -0400

    feat function returns score for a quarterback

--- scoring.js
+++ scoring.js
@@ -1,6 +1,14 @@
-const calculateScore = () => {
-    if (this.position === 'QB') {
+calculateScore = (player) => {
+    let passing = player.stats.passing
+    let rushing = player.stats.rushing
+    // let receiving = this.stats.receiving
+    // let returnKick = this.stats.return.kickreturn
+    // let returnPunt = this.stats.return.puntreturn
+    // let fieldgoals = this.stats.fieldgoals
+    // let xp = this.stats.xp
 
+    if (player.position === 'QB') {
+        return (passing.yards / 25) + (passing.touchdowns * 6) + (passing.interceptions * -3) + (rushing.yards / 10) + (rushing.touchdowns * 6) + (rushing.fumbles * -3)
     } else if (this.position === 'RB') {
 
     } else if (this.position === 'WR') {
