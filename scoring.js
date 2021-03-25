c0c92ef14af32f884d14b8a95821e4908d0c54fc
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-25 16:06:35 -0400

    add variables to reduce function size

--- scoring.js
+++ scoring.js
@@ -1,19 +1,19 @@
 calculateScore = (player) => {
     let passing = player.stats.passing
     let rushing = player.stats.rushing
-    // let receiving = this.stats.receiving
-    // let returnKick = this.stats.return.kickreturn
-    // let returnPunt = this.stats.return.puntreturn
-    // let fieldgoals = this.stats.fieldgoals
-    // let xp = this.stats.xp
+    let receiving = this.stats.receiving
+    let returnKick = this.stats.return.kickreturn
+    let returnPunt = this.stats.return.puntreturn
+    let fieldgoals = this.stats.fieldgoals
+    let xp = this.stats.xp
 
     if (player.position === 'QB') {
         return (passing.yards / 25) + (passing.touchdowns * 6) + (passing.interceptions * -3) + (rushing.yards / 10) + (rushing.touchdowns * 6) + (rushing.fumbles * -3)
-    } else if (this.position === 'RB') {
+    } else if (player.position === 'RB') {
 
-    } else if (this.position === 'WR') {
+    } else if (player.position === 'WR') {
 
-    } else if (this.position === 'TE') {
+    } else if (player.position === 'TE') {
 
     }
     return 0;
