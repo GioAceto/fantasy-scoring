1243234793dfd7e1555572f26352f2a6f0019da0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-26 18:15:33 -0400

    restructed and linted code for readability

--- scoring.js
+++ scoring.js
@@ -1,24 +1,23 @@
-calculateScore = (player) => {
-    let result;
+const calculateScore = (player) => {
+  let result
 
-    if (player.position === 'QB') {
-        let passing = player.stats.passing;
-        let rushing = player.stats.rushing;
+  if (player.position === 'QB') {
+    let passing = player.stats.passing
+    let rushing = player.stats.rushing
 
-        result = (passing.yards / 25) +
+    result = (passing.yards / 25) +
             (passing.touchdowns * 6) +
             (passing.interceptions * -3) +
             (rushing.yards / 10) +
             (rushing.touchdowns * 6) +
             (rushing.fumbles * -3)
+  } else if (player.position === 'RB') {
+    let rushing = player.stats.rushing
+    let receiving = player.stats.receiving
+    let kickReturn = player.stats.return.kickreturn
+    let puntReturn = player.stats.return.puntreturn
 
-    } else if (player.position === 'RB') {
-        let rushing = player.stats.rushing;
-        let receiving = player.stats.receiving;
-        let kickReturn = player.stats.return.kickreturn;
-        let puntReturn = player.stats.return.puntreturn;
-
-        result = (rushing.yards / 10) +
+    result = (rushing.yards / 10) +
             (rushing.touchdowns * 6) +
             (rushing.fumbles * -3) +
             (receiving.receptions) +
@@ -31,14 +30,13 @@ calculateScore = (player) => {
             (puntReturn.yards / 15) +
             (puntReturn.touchdowns * 6) +
             (puntReturn.fumbles * -3)
+  } else if (player.position === 'WR') {
+    let rushing = player.stats.rushing
+    let receiving = player.stats.receiving
+    let kickReturn = player.stats.return.kickreturn
+    let puntReturn = player.stats.return.puntreturn
 
-    } else if (player.position === 'WR') {
-        let rushing = player.stats.rushing;
-        let receiving = player.stats.receiving;
-        let kickReturn = player.stats.return.kickreturn;
-        let puntReturn = player.stats.return.puntreturn;
-
-        result = (rushing.yards / 10) +
+    result = (rushing.yards / 10) +
             (rushing.touchdowns * 6) +
             (rushing.fumbles * -3) +
             (receiving.receptions) +
@@ -51,20 +49,18 @@ calculateScore = (player) => {
             (puntReturn.yards / 15) +
             (puntReturn.touchdowns * 6) +
             (puntReturn.fumbles * -3)
+  } else if (player.position === 'TE') {
+    let receiving = player.stats.receiving
 
-    } else if (player.position === 'TE') {
-        let receiving = player.stats.receiving;
-
-        result = (receiving.receptions) +
+    result = (receiving.receptions) +
             (receiving.yards / 10) +
             (receiving.touchdowns * 6) +
             (receiving.fumbles * -3)
+  } else {
+    result = 0
+  }
 
-    } else {
-        result = 0;
-    }
-
-    return result;
+  return result
 }
 
 module.exports = calculateScore
